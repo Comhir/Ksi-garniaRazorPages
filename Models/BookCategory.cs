5bd81fb273ab5e73f527cae4e11f38b8409c1999
Adam Karpiesiuk <karpiesiuk.a@gmail.com>
2023-06-15 00:04:32 +0200

    Klasy obiektow

--- Models/BookCategory.cs
+++ Models/BookCategory.cs
@@ -2,5 +2,9 @@
 {
     public class BookCategory
     {
+        public int BookId { get; set; }
+        public int CategoryId { get; set; }
+        public Book Book { get; set; }
+        public Category Category { get; set; }
     }
 }
